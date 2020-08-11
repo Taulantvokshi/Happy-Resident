6fbecd5929a213adbb2f1f561b8d595d62517a51
Taulant Vokshi <taulantvokshi2@gmail.com>
2020-08-10 20:43:15 -0400

    change

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 tar czf happyR.tar.gz .babelrc .gitignore client package-lock.json package.json public script secrets.js server webpack.config.js
-scp happyR.tar.gz IP :~
+scp happyR.tar.gz 165.227.82.241 :~
 rm happyR.tar.gz
 
 
-ssh IP << 'ENDSSH'
+ssh 165.227.82.241 << 'ENDSSH'
 pm2 stop all
 rm -rf happyResident
 mkdir happyResident
@@ -15,7 +15,7 @@ npm install
 pm2 start all
 ENDSSH 
 
-ssh root@ IP  << 'ENDSSH'
+ssh root@ 165.227.82.241  << 'ENDSSH'
 setsebool -P httpd_can_network_connect on
 setsebool -P httpd_enable_homedirs on
 chcon -Rt httpd_sys_content_t /home/taulantvokshi/happyResident/public
